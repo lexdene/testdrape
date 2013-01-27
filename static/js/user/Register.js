1fc1ce5d7c09ff2c30e2bd819211a4ae7a3f2d9f
jecat_elephant@sina.cn <jecat_elephant@sina.cn@3aab84ea-45f4-4ca6-9053-3a61a15e3b4a>
2013-01-27 03:45:18 +0000

    user register
    git-svn-id: https://svn.sinaapp.com/testdrape/1@62 3aab84ea-45f4-4ca6-9053-3a61a15e3b4a

--- static/js/user/Register.js
+++ static/js/user/Register.js
@@ -11,7 +11,7 @@ register.validate_repassword = function(o,val,vil){
 			msg:'两次输入密码不一致',
 		}
 	}
-}
+};
 (function(jq,jw){
 	jq(function(){
 		jq('#register_form').submit(function(){
@@ -23,7 +23,7 @@ register.validate_repassword = function(o,val,vil){
 			form.ajaxSubmit({
 				'success':function(){
 					alert('登录成功');
-					window.location = WEB_ROOT + form.attr('redirect');
+					// window.location = WEB_ROOT + form.attr('redirect');
 				},
 				'failed':function(msg){
 					alert('登录失败:'+msg);
