e66bac9712d98f3c71c6001a6fcb367babf0f3cc
jecat_elephant@sina.cn <jecat_elephant@sina.cn@3aab84ea-45f4-4ca6-9053-3a61a15e3b4a>
2013-01-27 03:32:53 +0000

    user register
    git-svn-id: https://svn.sinaapp.com/testdrape/1@54 3aab84ea-45f4-4ca6-9053-3a61a15e3b4a

--- static/js/user/register.js
+++ static/js/user/register.js
@@ -12,3 +12,24 @@ register.validate_repassword = function(o,val,vil){
 		}
 	}
 }
+(function(j,jw){
+	j(function(){
+		document.getElementById('register_form').onsubmit=function(){
+			var v = jdmd_widget.validate_and_error_all( this );
+			if( false == v ){
+				return false;
+			}
+			var form = j(this);
+			form.ajaxSubmit({
+				'success':function(){
+					alert('登录成功');
+					window.location = WEB_ROOT + form.attr('redirect');
+				},
+				'failed':function(msg){
+					alert('登录失败:'+msg);
+				}
+			});
+			return false;
+		}
+	});
+})(jQuery,jdmd_widget);
