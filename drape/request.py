9d4798472c45155b315d17d2947022853b4662d8
jecat_elephant@sina.cn <jecat_elephant@sina.cn@3aab84ea-45f4-4ca6-9053-3a61a15e3b4a>
2013-03-24 02:07:22 +0000

    remove debug message from request
    git-svn-id: https://svn.sinaapp.com/testdrape/1@186 3aab84ea-45f4-4ca6-9053-3a61a15e3b4a

--- drape/request.py
+++ drape/request.py
@@ -3,9 +3,6 @@ class Request(object):
 		self.__controllerPath = None
 		
 	def run(self,params,env):
-		import debug
-		debug.debug(params)
-		debug.debug(env)
 		# url path
 		self.__urlPath = params.get('path')
 		qs = env.get('QUERY_STRING')
